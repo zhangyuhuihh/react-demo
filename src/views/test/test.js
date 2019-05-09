456a80105daee7167fd3ba6393f45a06d56ace07
zhangyuhui <842403078@qq.com>
2019-05-09 20:08:37 +0800

    添加antd前端ui框架

--- src/views/test/test.js
+++ src/views/test/test.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import Testt from './testt'
+// import Testt from './testt'
 import moduleCss from './testScss.module.scss'
+import { Button } from 'antd'
 export default class Test extends React.Component {
   render() {
     return (
       <div className={moduleCss.container}>
-      哈哈
+        哈哈
         {/* <h1 className={moduleCss.title}>这里是测试页面</h1> */}
-        <Testt></Testt>
+        {/* <Testt></Testt>
+         */}
+        <Button type="primary">Button</Button>
       </div>
     )
   }
