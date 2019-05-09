ea2f64c675e8d7540275e0a3b8f4c8bcb445fbe8
zhangyuhui <842403078@qq.com>
2019-05-09 11:55:45 +0800

    样式组件之间分开

--- src/views/test/test.js
+++ src/views/test/test.js
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import Testt from './testt'
+import moduleCss from './testScss.module.scss'
 export default class Test extends React.Component {
   render() {
-    return <h1>这里是测试页面</h1>
+    return (
+      <div className={moduleCss.container}>
+      哈哈
+        {/* <h1 className={moduleCss.title}>这里是测试页面</h1> */}
+        <Testt></Testt>
+      </div>
+    )
   }
 }
