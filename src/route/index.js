2b3ab567f4c859f5d9d01cf8503bb1b785ec3fb6
zhangyuhui <842403078@qq.com>
2019-07-04 15:13:35 +0800

    主体部分的路由权限完成，等待引入redux重构

--- src/route/index.js
+++ src/route/index.js
@@ -4,13 +4,40 @@ import Test from '@/views/test/test'
 import Testt from '@/views/test/testt'
 
 export default class MyRouter extends React.Component {
+  state = {
+    authArr: ['测试', '测试2'], // todo,放入redux进行权限判断
+    routeList: [
+      {
+        path: '/test',
+        component: Test,
+        role: '测试'
+      },
+      {
+        path: '/Testt',
+        component: Testt,
+        role: '测试2'
+      }
+    ]
+  }
+
   render() {
     return (
       <Switch>
         <Redirect exact from="/" to="/test" />
-        <Route path="/test" component={Test} />
-        <Route path="/Testt" component={Testt} />
+        {this.state.routeList
+          .filter(v => {
+            return this.hasPermission(v)
+          })
+          .map(v => {
+            return (
+              <Route path={v.path} component={v.component} key={v.path} />
+            )
+          })}
       </Switch>
     )
+  }
+
+  hasPermission(v) {
+    return this.state.authArr.includes(v.role)
   }
 }
